fcd63ca5894502c3130fc4afc22f3b0d68faf252
SemihKc1486 <semih.koc@restajet.com>
2023-05-24 11:15:02 +0300

    code cleanup

--- src/navigation/rootNavigation.tsx
+++ src/navigation/rootNavigation.tsx
@@ -5,29 +5,10 @@ import AppNavigation from './appNavigation';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import {  authentication } from '../store/slices/authSlice';
 import { useAppSelector } from '../store/hooks';
-// import {useTheme} from '../theme';
 import {navigationRef} from './navigationService';
-import { CredentialsContext } from '../components/credentialContext';
 export const RootNavigation = () => {
 
   const auth = useAppSelector(authentication);
-  const checkLoginCredentials = () => {
-    AsyncStorage.getItem('isLoggedin')
-    .then((result) => { 
-      if(result !== null) {
-
-        setStoredCredentials(result)
-
-      } else {
-
-      }
-    })
-    .catch(error => console.log(error))
-  }
-  
-  useEffect(() => {
-    checkLoginCredentials()
-  },[])
 
   return (
     <NavigationContainer ref={navigationRef}>
